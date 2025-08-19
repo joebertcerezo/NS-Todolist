c289fc0d6f6bb0b05bdf08b9164c0d5ca5844092
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-08-19 11:59:11 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['@nuxtjs/strapi', 'shadcn-nuxt', '@vueuse/nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+
+  runtimeConfig: {
+    strapi: {
+      url: 'http://localhost:1337'
+    },
+    public: {
+      strapi: {
+        url: 'http://localhost:1337'
+      }
+    }
+  },
 })
