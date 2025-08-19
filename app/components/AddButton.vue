77f800acc4fa247a3b84bbc2b902e8fdd3d90edc
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-08-19 11:59:02 +0800

    Create AddButton.vue

---
+++ app/components/AddButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="shadow-[0px_2px_8px_0px_rgba(99,99,99,0.2)] h-10 w-10 flex items-center justify-center rounded-full text-xl p-0 hover:bg-slate-200 hover:cursor-pointer"
+  >+</button>
+</template>
+
+<script lang="ts" setup></script>
