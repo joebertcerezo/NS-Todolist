d0d8eda184f352e60004b69bf21b72b33c019e5c
Joebert L. Cerezo <joebertcerezo@gmail.com>
2025-08-19 14:15:52 +0800

    Update AddButton.vue

--- app/components/AddButton.vue
+++ app/components/AddButton.vue
@@ -1,7 +1,8 @@
 <template>
-  <button
-    class="shadow-[0px_2px_8px_0px_rgba(99,99,99,0.2)] h-10 w-10 flex items-center justify-center rounded-full text-xl p-0 hover:bg-slate-200 hover:cursor-pointer"
-  >+</button>
+  <Button
+    class="shadow-[0px_2px_8px_0px_rgba(99,99,99,0.2)] h-10 w-10 flex items-center justify-center rounded-full text-xl p-0 hover:cursor-pointer"
+    >+
+  </Button>
 </template>
 
 <script lang="ts" setup></script>
